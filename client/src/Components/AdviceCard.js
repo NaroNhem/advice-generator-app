bd405a096f57be6ca94bddaa65dc9e3c5bc0fefc
Justin <n.justinaro@gmail.com>
2023-03-08 18:00:47 -0500

    finished product

--- client/src/Components/AdviceCard.js
+++ client/src/Components/AdviceCard.js
@@ -1,18 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export default function AdviceCard() {
   const [advice, setAdvice] = useState(null);
 
+  const fetchData = useCallback(async () => {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const { slip } = await response.json();
+    setAdvice(slip);
+  }, []);
+
   useEffect(() => {
-    console.log("before");
-    const fetchData = async () => {
-      const response = await fetch("https://api.adviceslip.com/advice");
-      const { slip } = await response.json();
-      setAdvice(slip);
-    };
     fetchData();
-  }, [setAdvice]);
-  console.log(advice);
+  }, [fetchData]);
+
+  const fetchAdvice = () => {
+    fetchData();
+  };
+
   return (
     <div className="card">
       {advice === null ? (
@@ -22,7 +26,7 @@ export default function AdviceCard() {
           <p>A D V I C E # {advice.id}</p>
           <h4>{advice.advice}</h4>
           <img src="/images/pattern-divider-desktop.svg" alt="divider" />
-          <button className="diceBtn">
+          <button className="diceBtn" onClick={fetchAdvice}>
             <img src="/images/icon-dice.svg" alt="dice" className="dice" />
           </button>
         </>
